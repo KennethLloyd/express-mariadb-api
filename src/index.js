1e145327473a7e74cad9a4b5ba6109b9d9515e74
KennethLloyd <verastiguelloyd@gmail.com>
2021-03-03 20:36:07 +0800

    Fix apiDoc issue

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import initDB from './db/initialize.js';
 import initRouters from './routers/index.js';
 
 const app = express();
 const port = process.env.PORT || 3001;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json()); // allows us to parse the request as json
 app.use(cors());
-app.use('/apidoc', express.static(path.join(import.meta.url, '../docs')));
+app.use('/apidoc', express.static(path.join(__dirname, '../docs')));
 
 (async () => {
   try {
